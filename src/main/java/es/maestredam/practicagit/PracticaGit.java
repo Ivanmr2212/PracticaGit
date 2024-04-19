07990dbb6949cfd515a22d89f4fe3be0da5741dc
ivan <imartinr29@educacastillalamancha.es>
2024-04-19 10:19:13 +0200

    Punto 6 hecho de nuevo
    No hice captura

--- src/main/java/es/maestredam/practicagit/PracticaGit.java
+++ src/main/java/es/maestredam/practicagit/PracticaGit.java
@@ -14,5 +14,7 @@ public class PracticaGit {
         System.out.println("Practica Github");
         System.out.println("");
         System.out.println("Cambios hechos para el punto 6");
+        System.out.println("");
+        System.out.println("Mas cambios porque no he hecho captura");
     }
 }
