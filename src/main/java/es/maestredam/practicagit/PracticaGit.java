cc04b23ed553056a977246b21726798347461f6c
ivan <imartinr29@educacastillalamancha.es>
2024-04-19 10:14:34 +0200

    Initial commit

---
+++ src/main/java/es/maestredam/practicagit/PracticaGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.maestredam.practicagit;
+
+/**
+ *
+ * @author ivanm
+ */
+public class PracticaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Practica Github");
+    }
+}
